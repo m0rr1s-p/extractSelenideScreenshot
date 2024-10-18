cf8e08d7339652fc428437bb80ac3db56bae6bd7
Morris <morris.peichl@mineral-minds.de>
2024-10-18 13:03:11 +0200

    test 1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,45 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
+
   try {
-    const ms: string = core.getInput('milliseconds')
+    core.debug('Get input for "gh-token"')
+    const ghToken: string = core.getInput('gh-token', {required: true})
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug('Get octokit instance')
+    const octokit = github.getOctokit(ghToken)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repoOwner = core.getInput('repo-owner')
+    core.debug(`Repo owner: ${repoOwner}`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const repoName = core.getInput('repo-name').replace(`${repoOwner}/`, '')
+    core.debug(`Repo name: ${repoName}`)
+
+    core.debug(`Job ID ${core.getInput('run-id')}`)
+    core.debug('Getting workflow jobs')
+    const resJobs = await octokit.rest.actions.listJobsForWorkflowRun({
+      run_id: Number(core.getInput('run-id')),
+      owner: repoOwner,
+      repo: repoName
+    })
+
+    const job = resJobs.data.jobs.filter(
+      val => val.name === core.getInput('job-name')
+    )
+
+    core.debug(`Job ID: ${job[0].id}`)
+
+    core.debug('Getting workflow logs')
+    const errorLogs = await octokit.rest.actions.downloadJobLogsForWorkflowRun({
+      job_id: job[0].id,
+      owner: repoOwner,
+      repo: repoName
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
