376d3365f103143652ca358e7358012f05a5b26d
Morris <morris.peichl@mineral-minds.de>
2025-01-16 16:39:11 +0100

    chevereto with curl

--- src/upload.ts
+++ src/upload.ts
@@ -1,32 +1,31 @@
-import * as core from '@actions/core'
-import { v2 as cloudinary } from 'cloudinary'
+//import * as core from '@actions/core'
+import * as child_process from 'child_process'
 //import * as path from 'path'
 
-export async function uploader(
-  cloudName: string | undefined,
+//export async function uploader(
+//  hostingUrl: string | undefined,
+//  apiKey: string | undefined,
+//  paths: string[]
+//): Promise<void> {
+//  cloudinary.config({
+//    cloud_name: cloudName,
+//    api_key: apiKey,
+//    api_secret: apiSecret
+//  })
+//  for (const path of paths) {
+//    const { exec } = require('child_process');
+//    exec(`curl --fail-with-body -X POST -H "X-API-Key: $\{api_key}" -F "source=@$\{path}" ${hostingUrl}`)
+//  }
+//}
+
+export function uploader(
+  hostingUrl: string | undefined,
   apiKey: string | undefined,
-  apiSecret: string | undefined,
   paths: string[]
-): Promise<void> {
-  cloudinary.config({
-    cloud_name: cloudName,
-    api_key: apiKey,
-    api_secret: apiSecret
-  })
+): void {
   for (const path of paths) {
-    cloudinary.uploader
-      .upload(path, {
-        use_filename: true,
-        overwrite: true
-      })
-      .then(result => {
-        core.summary
-          .addHeading('Selenide Screenshots', '2')
-          .addImage(result.secure_url, result.public_id)
-          .write()
-        core.info(
-          `Uploaded ${result.secure_url} as ${result.public_id} to Cloudinary`
-        )
-      })
+    child_process.exec(
+      `curl --fail-with-body -X POST -H "X-API-Key: ${apiKey}" -F "source=@${path}" ${hostingUrl}`
+    )
   }
 }
